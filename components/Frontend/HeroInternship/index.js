97e71e3d11bedcaae27e948c0fa4120cf0ed0542
ralph jularbal <ralphjularbal@yahoo.com>
2022-08-04 03:21:16 +0800

    showcase content and layout

--- components/Frontend/HeroInternship/index.js
+++ components/Frontend/HeroInternship/index.js
@@ -1,6 +1,7 @@
 import "./styles.scss";
 
 import { forwardRef } from "react";
+import { IoHappy, IoPeople, IoRibbon, IoBook } from "react-icons/io5";
 import Link from "next/link";
 
 const HeroInternship = forwardRef(({ data, black, blue, next }, myRef) => {
@@ -25,11 +26,57 @@ const HeroInternship = forwardRef(({ data, black, blue, next }, myRef) => {
           <div className="title">Internship Program</div>
           <div className="content">Not all internships are created equal.</div>
           <div className="cta-button">
-            <span>apply for internship</span> <img src="/common/arrow-white.svg" />
+            <span>apply for internship</span>{" "}
+            <img src="/common/arrow-white.svg" />
           </div>
         </div>
         <div className="hero-column">
-     
+          <div className="showcase">
+            <div className="showcase-item">
+              <div className="title">
+                <IoRibbon className="icon" />
+                Gain Experience
+              </div>
+              <div className="description">
+                Having an internship with us allows you to experience firsthand
+                how the media industry operates. It provides you the necessary
+                knowledge and experience to prepare you in your chosen
+                profession.
+              </div>
+            </div>
+            <div className="showcase-item">
+              <div className="title">
+                <IoBook className="icon" />
+                Learn from the Best in the Industry
+              </div>
+              <div className="description">
+                Having an internship with us gives you the opportunity to learn
+                from the best in the industry. Learn from established
+                personalities in both AM and FM station.
+              </div>
+            </div>
+            <div className="showcase-item">
+              <div className="title">
+                <IoPeople className="icon" />
+                Opportunity to be Part of the Team
+              </div>
+              <div className="description">
+                We give priority to our former interns whenever we have a job
+                vacancy. A significant number of our employees are former
+                interns.
+              </div>
+            </div>
+            <div className="showcase-item">
+              <div className="title">
+                <IoHappy className="icon" />
+                Have Fun While Working
+              </div>
+              <div className="description">
+                We make sure that you are also enjoying while you are working
+                and learning with us.
+              </div>
+            </div>
+          </div>
         </div>
 
         <div className="arrow-placement">
