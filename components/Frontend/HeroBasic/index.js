8bcb783af48711e40b05c507d9f20113ce9fd4b1
ralph jularbal <ralphjularbal@yahoo.com>
2022-08-11 21:33:33 +0800

    add CTA option

--- components/Frontend/HeroBasic/index.js
+++ components/Frontend/HeroBasic/index.js
@@ -1,7 +1,7 @@
 import "./styles.scss";
 
 import { forwardRef, useState } from "react";
-
+import Link from "next/link";
 const HeroBasic = forwardRef(({ data, black, blue, next }, myRef) => {
   const test = {
     bgLocation:
@@ -28,6 +28,13 @@ const HeroBasic = forwardRef(({ data, black, blue, next }, myRef) => {
         {blue && <div className="bg-overlay-blue" />}
         <div className="title">{data.title}</div>
         <div className="content">{data.content}</div>
+        {data.ctaText && data.ctaLink && (
+          <Link href={data.ctaLink}>
+            <button className="cta-button btn btn-block">
+              <span>{data.ctaText}</span> <img src="/common/arrow-white.svg" />
+            </button>
+          </Link>
+        )}
         <div className="arrow-placement">
           <div className="arrow-wrapper" onClick={scrollNext} />
         </div>
