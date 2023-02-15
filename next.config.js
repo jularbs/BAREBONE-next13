e94fe479acb5c1716001bfa91e976d06df4b5265
Ralph Jularbal <ralphjularbal@Ralphs-MacBook-Pro-2.local>
2023-02-15 23:31:29 +0800

    add security headers

--- next.config.js
+++ next.config.js
@@ -2,6 +2,50 @@ const path = require("path");
 const withImage = require("next-images");
 
 const nextConfig = {
+  headers: () => {
+    return [
+      {
+        source: "/:path*{/}?",
+        headers: [
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=31536000; includeSubDomains",
+          },
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self';" +
+              "font-src 'self' data: fonts.gstatic.com;" +
+              "style-src 'self' 'unsafe-inline' cdn.tiny.cloud fonts.googleapis.com github.hubspot.com www.google.com;" +
+              "script-src 'self' 'unsafe-eval' 'unsafe-inline' github.hubspot.com cdn.tiny.cloud www.google.com www.gstatic.com *.youtube.com github.hubspot.com;" +
+              "connect-src 'self' localhost:8000 github.hubspot.com www.google.com;" +
+              "img-src 'self' data: sp.tinymce.com localhost:8000;" +
+              "frame-src 'self' www.google.com *.youtube.com;" +
+              "frame-ancestors 'self';" +
+              "media-src 'self';" +
+              "object-src 'self';",
+          },
+          {
+            key: "Permissions-Policy",
+            value: "camera=(), microphone=(), geolocation=(self)",
+          },
+
+          {
+            key: "X-Frame-Options",
+            value: "sameorigin",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+        ],
+      },
+    ];
+  },
   reactStrictMode: false,
   poweredByHeader: false,
   env: {
